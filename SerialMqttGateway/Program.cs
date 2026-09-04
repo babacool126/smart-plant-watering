342d6afd003f60085bf1305f693244dac265c97c
babacool126 <babacool126@gmail.com>
2026-09-04 17:35:21 +0200

    TAIGA-3: add initial MQTT gateway client

---
+++ SerialMqttGateway/Program.cs
@@ -0,0 +1,26 @@
+﻿using MQTTnet;
+
+var mqttFactory = new MqttClientFactory();
+
+using var mqttClient = mqttFactory.CreateMqttClient();
+
+var mqttClientOptions = new MqttClientOptionsBuilder()
+    .WithTcpServer("node-01.lab.thomaslab.nl", 1883)
+    .Build();
+
+Console.WriteLine("Verbinden met MQTT broker...");
+
+await mqttClient.ConnectAsync(mqttClientOptions);
+
+Console.WriteLine("Verbonden.");
+
+var message = new MqttApplicationMessageBuilder()
+    .WithTopic("plant/test")
+    .WithPayload("hello from C#")
+    .Build();
+
+await mqttClient.PublishAsync(message);
+
+Console.WriteLine("Bericht gepubliceerd.");
+
+await mqttClient.DisconnectAsync();
