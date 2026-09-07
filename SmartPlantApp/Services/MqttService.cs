bfd001f92e5dfab80c6a530a0d43d3ba28aca126
babacool126 <babacool126@gmail.com>
2026-09-07 15:06:55 +0200

    TAIGA-39: subscribe to moisture MQTT topic

--- SmartPlantApp/Services/MqttService.cs
+++ SmartPlantApp/Services/MqttService.cs
@@ -10,6 +10,16 @@ public class MqttService
     {
         var factory = new MqttClientFactory();
         _mqttClient = factory.CreateMqttClient();
+
+        _mqttClient.ApplicationMessageReceivedAsync += e =>
+        {
+            var topic = e.ApplicationMessage.Topic;
+            var payload = e.ApplicationMessage.ConvertPayloadToString();
+
+            Console.WriteLine($"MQTT message received on {topic}: {payload}");
+
+            return Task.CompletedTask;
+        };
     }
 
     public async Task ConnectAsync()
@@ -22,5 +32,16 @@ public class MqttService
         var result = await _mqttClient.ConnectAsync(options);
 
         Console.WriteLine($"MQTT connection result: {result.ResultCode}");
+        var subscribeOptions = new MqttClientSubscribeOptionsBuilder()
+            .WithTopicFilter(f =>
+            {
+                f.WithTopic("plant/sensors/moisture");
+            })
+            .Build();
+
+        await _mqttClient.SubscribeAsync(subscribeOptions);
+
+        Console.WriteLine("Subscribed to plant/sensors/moisture");
+
     }
 }
